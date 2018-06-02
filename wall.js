d254d57fa192645a8f51ae2380aebdd8390fc55f
Tom <tom@ftml.net>
2018-06-02 16:50:08 +0200

    Initial commit

---
+++ wall.js
@@ -0,0 +1,56 @@
+'use strict'
+
+var GameComponent = {
+  props: ['game'],
+  template:
+    `<div class="cell">
+      <div class="game">
+      <a :href="gameUrl">
+        <div class=thumb>          
+            <img :src="game.thumbnail"/>          
+        </div>
+        <div class="label">
+          {{game.name}}
+        </div>
+      </a>
+      </div>
+    </div>`,
+    computed: {
+      gameUrl(){
+        return `https://boardgamegeek.com/boardgame/${this.game.gameId}`
+      }
+    }
+}
+
+const gameVue = new Vue({
+  el: '#wall',
+  data: {
+    loaded: false,
+    games: []
+  },
+  components: {
+    game: GameComponent
+  },
+  methods: {
+    getUrlParam(paramName) {
+      var searchParams = new URLSearchParams(window.location.search)
+      return searchParams.get(paramName)
+    }
+  },
+  mounted() {
+    let user = this.getUrlParam('user')
+    document.title = `BGG Wall - ${user}'s collection`
+    fetchJsonp(`https://bgg-json.azurewebsites.net/collection/${user}`)
+      .then(response => {
+        return response.json()
+      })
+      .then(json => {
+        // keep only owned games
+        this.games = json.filter(g => {return g.owned})
+        this.loaded = true
+      })
+      .catch(function(error) {
+        alert('Could not load games from server.\n\n' + error)
+      })
+  }
+})
